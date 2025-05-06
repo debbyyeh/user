6ffb9ea75f7db185dceb35929cab93ff9489b713
debbyyeh <app2984@gmail.com>
2025-05-06 20:53:26 +0800

    set up global css

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,18 @@
-import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import { Pagination } from './components/pagination/Pagination';
+import { Content } from './components/content/Content';
+
 
 function App() {
+
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className="dropdown">
+        <div>用戶管理</div>
+      </div>
+      <Content />
+      <Pagination />
     </div>
   );
 }
